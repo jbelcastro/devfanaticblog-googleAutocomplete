10921cb0dc33753b14ee2bfaaf379c01a82cd6c8
Ivan Larionov <creminds@gmail.com>
2016-08-27 14:01:39 +0400

    done

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,25 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, ModalController} from 'ionic-angular';
+import {AutocompletePage} from './autocomplete';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
+  address;
+
+  constructor(private navCtrl: NavController, private modalCtrl: ModalController) {
+    this.address = {
+      place: ''
+    };
+  }
   
+  showAddressModal () {
+    let modal = this.modalCtrl.create(AutocompletePage);
+    let me = this;
+    modal.onDidDismiss(data => {
+      this.address.place = data;
+    });
+    modal.present();
   }
 }
